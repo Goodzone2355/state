6bd702e7731153326cb072fc6ea816985a7987f4
Goodzone2355 <chulskij1996@gmail.com>
2023-06-20 15:02:15 +0300

    labudilabudai

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Album from './components/music';
+import Portfolio from './components/portfolio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Album
+        img = '/img/ram.jpeg'
+        nameAlbum = 'Made in Germany 1995–2011'
+        nameGroup = 'Rammstein'
+        year = {2011}
+      />
+
+      <Portfolio
+        img = '/img/ава1.jpg'
+        name = 'Чульский Владимир Вадимович'
+        telephone = '8-800-555-35-35'
+        email = 'volodenka2355@gmail.com'
+        town = 'Донецк'
+        experiences = {['2024-2028 - обучение в бакалавриате ЮФУ на кафедре "Прикладная математика и информатика"', '2028-2030 - обучение в магистратуре ЮФУ на кафедре "Прикладная математика и информатика"']}
+      />
     </div>
   );
 }
